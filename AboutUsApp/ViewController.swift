9353952e9947892c442d987877ba27bce3e4f837
Oleg <oleg.zuev.2012@gmail.com>
2025-06-05 20:17:44 +0300

    Initial Commit

---
+++ AboutUsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutUsApp
+//
+//  Created by Олег Зуев on 05.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
